b3dd974ce96656fb5d4080bd2bf8364f1879f521
Foulbazar <94230316+Foulbazar@users.noreply.github.com>
2024-02-06 11:23:24 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,31 +5,34 @@
 # chmod +x gophish_image_setup.sh
 # ./gophish_image_setup.sh phisher.com,www.phisher.com
 
-
-# Check if the required parameters are provided
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]
-then
-  echo "Usage: $0 -d <domain_name> -ip <ip_address> -m [complete|partial]"
-  exit 1
+# Check if the correct number of arguments are provided
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 {setup|update} [domain_name] [ip]"
+    echo "       $0 setup <domain_name> <ip>"
+    echo "       $0 update <old_domain> <new_domain>"
+    exit 1
 fi
-domain_name=$1
-IP=$2
-mode=$3
-
-if [ "$mode" = "complete" ]
-then
+# Set variables based on the first argument
+if [ "$1" = "setup" ]; then
+    MODE="setup"
+    domain_name="$2"
+    IP="$3"
     # Install Prerequisites
     sudo apt -y update; sudo apt -y upgrade
     sudo apt -y install jq build-essential certbot
     sudo apt -y install sqlite3
+    sudo apt -y install snap
     sudo apt-get install -qq -y opendkim opendkim-tools
     sudo apt-get install -qq -y mailutils
     
     # Setup the Firewall
-    ufw allow from $IP to any port 22
-    ufw allow from $IP to any port 3333
-    ufw allow 443
-    ufw allow 80
+    sudo ufw default deny incoming   
+    sudo ufw default allow ongoing
+    sudo ufw allow from $IP to any port 22
+    sudo ufw allow from $IP to any port 3333
+    sudo ufw allow 443
+    sudo ufw allow 80
+    sudo ufw enable
 
     # Install go
     VERSION=$(curl -s https://go.dev/dl/?mode=json | jq -r '.[0].version')
@@ -80,6 +83,57 @@ then
     hostnamectl
     hostnamectl set-hostname $domain_name --pretty
     hostnamectl ##Pour confirmer les changements
+
+    # Install postfix
+    sudo apt-get install -y postfix
+    cd /etc/postfix/main.cf
+
+    # Modify the config files /etc/postfix/generic
+    current_domain=$(grep '^myhostname' /etc/postfix/main.cf | awk '{ print $2 }')
+    sudo sed -i "s/$current_domain/$domain_name/g" /etc/postfix/generic
+
+    # Modify the /etc/postfix/main.cf file
+    sudo sed -i "s/$current_domain/$domain_name/g" /etc/postfix/main.cf
+
+    # Configure Certificate
+    sudo snap install --classic certbot
+    sudo ln -s /snap/bin/certbot /usr/bin/certbot
+    sudo certbot certonly --standalone -d $domain_name   
+    cd Gophish/ ##Sur le postfix  
+    sudo adduser ubuntu certs
+    sudo chgrp -R certs /etc/letsencrypt/
+    sudo chmod -R g+rx /etc/letsencrypt/
+    sudo cat /etc/letsencrypt/live/$domain_name/fullchain.pem  
+    cp /etc/letsencrypt/live/$domain_name/fullchain.pem $domain_name.crt  
+    cp /etc/letsencrypt/live/$domain_name/privkey.pem $domain_name.key
+
+    # Configure DKIM
+    sudo mkdir -p /etc/opendkim/keys
+    sudo chown -R opendkim:opendkim /etc/opendkim
+    sudo chmod 744 /etc/opendkim/keys
+    sudo mkdir /etc/opendkim/keys/$dommain_name
+    sudo opendkim-genkey -b 2048 -d $dommain_name -D /etc/opendkim/keys/$dommain_name -s default -v
+    sudo chown opendkim:opendkim /etc/opendkim/keys/$dommain_name/default.private
+    private-key-dkim=$(sudo cat /etc/opendkim/keys/$nom_de_domaine/default.txt)
+
+elif [ "$1" = "update" ]; then
+    MODE="update"
+    old_domain="$2"
+    domain_name="$3"
+else
+    echo "Invalid mode. Use 'setup' or 'update'"
+    echo "Usage: $0 {setup|update} [domain_name] [ip]"
+    echo "       $0 setup <domain_name> <ip>"
+    echo "       $0 update <old_domain> <new_domain>"
+    exit 1
+fi 
+
+if [ "$mode" = "complete" ]
+then
+    
+
+    
+
 fi
 
 if [ "$mode" = "partial" ]
@@ -87,14 +141,6 @@ then
     systemctl stop gophish.service
     systemctl stop nginx
 fi
-
-
-
-
-
-
-
-
 
 
 # Generate SSL certificate
