3e79eeda3f68a6fd4ca9e527a04c2fce4b01730f
Foulbazar <94230316+Foulbazar@users.noreply.github.com>
2024-02-06 12:12:37 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@
 
 # Check if the correct number of arguments are provided
 if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 {setup|update} [domain_name] [ip]"
-    echo "       $0 setup <domain_name> <ip>"
+    echo "Usage: $0 {setup|update} [domain_name] [ip|new_domain]"
+    echo "       $0 setup <domain_name> <ip|new_domain>"
     echo "       $0 update <old_domain> <new_domain>"
     exit 1
 fi
@@ -17,6 +17,7 @@ if [ "$1" = "setup" ]; then
     MODE="setup"
     domain_name="$2"
     IP="$3"
+
     # Install Prerequisites
     sudo apt -y update; sudo apt -y upgrade
     sudo apt -y install jq build-essential certbot
@@ -98,73 +99,25 @@ if [ "$1" = "setup" ]; then
     # Configure Certificate
     sudo snap install --classic certbot
     sudo ln -s /snap/bin/certbot /usr/bin/certbot
-    sudo certbot certonly --standalone -d $domain_name   
-    cd Gophish/ ##Sur le postfix  
+    sudo certbot certonly --standalone -d $domain_name --agree-tos 
     sudo adduser ubuntu certs
     sudo chgrp -R certs /etc/letsencrypt/
     sudo chmod -R g+rx /etc/letsencrypt/
     sudo cat /etc/letsencrypt/live/$domain_name/fullchain.pem  
-    cp /etc/letsencrypt/live/$domain_name/fullchain.pem $domain_name.crt  
-    cp /etc/letsencrypt/live/$domain_name/privkey.pem $domain_name.key
 
-    # Configure DKIM
-    sudo mkdir -p /etc/opendkim/keys
-    sudo chown -R opendkim:opendkim /etc/opendkim
-    sudo chmod 744 /etc/opendkim/keys
-    sudo mkdir /etc/opendkim/keys/$dommain_name
-    sudo opendkim-genkey -b 2048 -d $dommain_name -D /etc/opendkim/keys/$dommain_name -s default -v
-    sudo chown opendkim:opendkim /etc/opendkim/keys/$dommain_name/default.private
-    private-key-dkim=$(sudo cat /etc/opendkim/keys/$nom_de_domaine/default.txt)
-
-elif [ "$1" = "update" ]; then
-    MODE="update"
-    old_domain="$2"
-    domain_name="$3"
-else
-    echo "Invalid mode. Use 'setup' or 'update'"
-    echo "Usage: $0 {setup|update} [domain_name] [ip]"
-    echo "       $0 setup <domain_name> <ip>"
-    echo "       $0 update <old_domain> <new_domain>"
-    exit 1
-fi 
-
-if [ "$mode" = "complete" ]
-then
-    
-
-    
-
-fi
-
-if [ "$mode" = "partial" ]
-then
-    systemctl stop gophish.service
-    systemctl stop nginx
-fi
-
-
-# Generate SSL certificate
-certbot certonly --expand -d $HOSTS -n --standalone --agree-tos --email info@onvio.nl
-
-unset -v latest
-for file in /etc/letsencrypt/live/*; do
-  [[ $file =~ README ]] && continue
-  [[ $file -nt $letsencryptPath ]] && letsencryptPath=$file
-done
-
-# Create config
-echo "{
+    # Create config
+    echo "{
         \"admin_server\": {
                 \"listen_url\": \"0.0.0.0:3333\",
                 \"use_tls\": true,
-                \"cert_path\": \"$letsencryptPath/fullchain.pem\",
-                \"key_path\": \"$letsencryptPath/privkey.pem\"
+                \"cert_path\": \"/etc/letsencrypt/live/$domain_name/fullchain.pem\",
+                \"key_path\": \"/etc/letsencrypt/live/$domain_name/privkey.pem\"
         },
         \"phish_server\": {
-                \"listen_url\": \"0.0.0.0:443\",
+                \"listen_url\": \"0.0.0.0:8080\",
                 \"use_tls\": true,
-                \"cert_path\": \"$letsencryptPath/fullchain.pem\",
-                \"key_path\": \"$letsencryptPath/privkey.pem\"
+                \"cert_path\": \"/etc/letsencrypt/live/$domain_name/fullchain.pem\",
+                \"key_path\": \"/etc/letsencrypt/live/$domain_name/privkey.pem\"
         },
         \"db_name\": \"sqlite3\",
         \"db_path\": \"gophish.db\",
@@ -173,4 +126,129 @@ echo "{
         \"logging\": {
                 \"filename\": \"\"
         }
-}" > $installPath/config.json
+    }" > $installPath/config.json
+
+    # Configure DKIM
+    sudo mkdir -p /etc/opendkim/keys
+    sudo chown -R opendkim:opendkim /etc/opendkim
+    sudo chmod 744 /etc/opendkim/keys
+    sudo mkdir /etc/opendkim/keys/$dommain_name
+    sudo opendkim-genkey -b 2048 -d $dommain_name -D /etc/opendkim/keys/$dommain_name -s default -v
+    sudo chown opendkim:opendkim /etc/opendkim/keys/$dommain_name/default.private
+    private=$(sudo cat /etc/opendkim/keys/$nom_de_domaine/default.txt)
+    echo "The private key for DKIM is :
+    \$private
+    "
+
+    # Configurer the nginx
+    sudo apt-get -y install nginx
+    echo "
+    server {
+        listen 443 ssl;
+        listen [::]:443;
+        server_name    $domain_name ;
+        add_header X-Robots-Tag "noindex, nofollow, nosnippet, noarchive";
+
+    if (\$http_user_agent\ ~* (google) ) {
+        return 404;
+	}
+  
+    if (\$http_user_agent\ = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"){
+        return 404;
+	}
+    }" > /etc/nginx/sites-enabled/$domain_name.conf
+
+    sudo service nginx reload
+    sudo certbot --nginx -d $domain_name
+    sudo service nginx reload
+
+    # Restart the services 
+    sudo systemctl restart gophish.service
+    sudo /usr/sbin/postmap /etc/postfix/generic 
+    sudo service postfix restart 
+
+elif [ "$1" = "update" ]; then
+    MODE="update"
+    old_domain="$2"
+    domain_name="$3"
+    systemctl stop gophish.service
+    systemctl stop nginx
+    systemctl stop postfix
+
+    # Add the domain name 
+    hostname $domain_name 
+    echo "$domain_name" > /etc/hostname  
+    echo "127.0.1.1 $domain_name" >> /etc/hosts
+
+    # Change the hostname
+    hostnamectl set-hostname $domain_name
+    hostnamectl
+    hostnamectl set-hostname $domain_name --pretty
+    hostnamectl ##Pour confirmer les changements
+
+    # Modify the postfix
+    sudo sed -i "s/$old_domain/$domain_name/g" /etc/postfix/generic
+
+    # Modify the /etc/postfix/main.cf file
+    sudo sed -i "s/$old_domain/$domain_name/g" /etc/postfix/main.cf
+    
+    # Generate the certificate
+    sudo certbot certonly --standalone -d $domain_name --agree-tos
+
+    # Modify the config file
+    echo "{
+        \"admin_server\": {
+                \"listen_url\": \"0.0.0.0:3333\",
+                \"use_tls\": true,
+                \"cert_path\": \"/etc/letsencrypt/live/$domain_name/fullchain.pem\",
+                \"key_path\": \"/etc/letsencrypt/live/$domain_name/privkey.pem\"
+        },
+        \"phish_server\": {
+                \"listen_url\": \"0.0.0.0:8080\",
+                \"use_tls\": true,
+                \"cert_path\": \"/etc/letsencrypt/live/$domain_name/fullchain.pem\",
+                \"key_path\": \"/etc/letsencrypt/live/$domain_name/privkey.pem\"
+        },
+        \"db_name\": \"sqlite3\",
+        \"db_path\": \"gophish.db\",
+        \"migrations_prefix\": \"db/db_\",
+        \"contact_address\": \"\",
+        \"logging\": {
+                \"filename\": \"\"
+        }
+    }" > /opt/gophish/config.json
+
+    # Modify the DKIM
+    sudo mkdir /etc/opendkim/keys/$dommain_name
+    sudo opendkim-genkey -b 2048 -d $dommain_name -D /etc/opendkim/keys/$dommain_name -s default -v
+    sudo chown opendkim:opendkim /etc/opendkim/keys/$dommain_name/default.private
+    private=$(sudo cat /etc/opendkim/keys/$nom_de_domaine/default.txt)
+    echo "The private key for DKIM is :
+    \$private
+    "
+
+    # Modify the nginx conf
+
+    echo "
+    server {
+        listen 443 ssl;
+        listen [::]:443;
+        server_name    $domain_name ;
+        add_header X-Robots-Tag "noindex, nofollow, nosnippet, noarchive";
+
+    if (\$http_user_agent\ ~* (google) ) {
+        return 404;
+	}
+  
+    if (\$http_user_agent\ = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"){
+        return 404;
+	}
+    }" > /etc/nginx/sites-enabled/$domain_name.conf
+
+else
+    echo "Invalid mode. Use 'setup' or 'update'"
+    echo "Usage: $0 {setup|update} [domain_name|old_domain] [ip|new_domain]"
+    echo "       $0 setup <domain_name> <ip|new_domain>"
+    echo "       $0 update <old_domain> <new_domain>"
+    exit 1
+fi
